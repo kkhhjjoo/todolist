854fba33800b8bfc40eb7da433001a87063c5ab8
hyunjoo.kim <khjoo1203@gmail.com>
2025-06-18 14:50:55 +0900

    추가 수정

--- main.js
+++ main.js
@@ -2,32 +2,119 @@
 //+ 버튼을 클릭하면, 할일이 추가된다
 //delete버튼을 누르면 할일이 삭제된다
 //check버튼을 누르면 할이이 끝나면서 밑줄이 간다
+//1. check버튼을 클릭하는 순간 false->true로 바꿔준다
+//2. true이면 끝난걸로 간주하고 밑줄 보여주기
+//3. false이면 안끝난걸로 간주하고 그대로
+
 //진행중 끝남 탭을 누르면, 언더바가 이동한다
 //완료 탭은 끝난 아이템만, 진행중 탭은 진행중 아이템만
 //전체 탭을 누르면 전체 아이템으로 돌아옴
 
 let taskInput = document.getElementById('task-input');
 let addButton = document.getElementById('add-button');
+let tabs = document.querySelectorAll('.task-tabs div');
+let underLine = document.getElementById('tab-underline');
 let taskList = [];
+let mode = 'all';
+let filterList = [];
 
-addButton.addEventListener('click', addTask);
-
+addButton.addEventListener('mousedown', addTask);
+taskInput.addEventListener('keydown', function (event) {
+  if (event.keyCode === 13) {
+    addTask(event);
+  }
+});
 function addTask() {
-  let taskContent = taskInput.value;
-  taskList.push(taskContent);
+  let taskValue = taskInput.value;
+  if (taskValue === '') return alert('할일을 입력해주세요');
+  let task = {
+    content: taskValue,
+    isComplete: false,
+    id: randomIDGenerate(),
+  };
+
+  taskList.push(task);
+  taskInput.value = '';
   render();
 }
+
 function render() {
-  let resultHTML = '';
-  for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `<div class="task">
-            <div>${taskList[i]}</div>
-            <div>
-              <button type="button">Check</button>
-              <button type="button">Delete</button>
-            </div>
-          </div>`;
+  let result = '';
+  list = [];
+  if (mode === 'all') {
+    list = taskList;
+  } else {
+    // else if(mode=== "ongoing" || mode==="done") 을 결국 else 로 치환할 수 있다.
+    list = filterList;
   }
 
-  document.getElementById('task-board').innerHTML = resultHTML;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete) {
+      result += `<div class="task task-done" id="${list[i].id}">
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fas fa-undo-alt"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>`;
+    } else {
+      result += `<div class="task" id="${list[i].id}" >
+            <span>${list[i].content}</span>
+            <div class="button-box">
+            <button onclick="toggleDone('${list[i].id}')"><i class="fa fa-check"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>`;
+    }
+  }
+
+  document.getElementById('task-board').innerHTML = result;
+}
+
+function toggleDone(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  filter();
+}
+
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList.splice(i, 1);
+    }
+  }
+
+  filter();
+}
+function filter(e) {
+  if (e) {
+    mode = e.target.id;
+    underLine.style.width = e.target.offsetWidth + 'px';
+    underLine.style.left = e.target.offsetLeft + 'px';
+    underLine.style.top =
+      e.target.offsetTop + (e.target.offsetHeight - 4) + 'px';
+  } // 진행중 상태에서 끝남으로 표시하면 바로 사라지는 부분은 event가 없음 그래서 조건추가
+
+  filterList = [];
+  if (mode === 'ongoing') {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filterList.push(taskList[i]);
+      }
+    }
+  } else if (mode === 'done') {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete) {
+        filterList.push(taskList[i]);
+      }
+    }
+  }
+  render();
+}
+function randomIDGenerate() {
+  return '_' + Math.random().toString(36).substring(2, 9);
 }
